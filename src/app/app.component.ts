46ce94cf32d353dce32b2cdcb3517ef2ad605c3d
GuiSantosMacedo1 <guisantosmacedo7@gmail.com>
2025-05-11 01:46:34 -0300

    feat:correções para post e get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,36 +2,64 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   userForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private userService: UserService) {
     this.userForm = this.fb.group({
       name: ['', [Validators.required]],
       age: ['', [Validators.required, Validators.min(1)]],
       sex: ['', [Validators.required]],
       document: ['', [Validators.required]],
-      address: ['', [Validators.required]]
+      status: [false, [Validators.required]],
+      address: this.fb.group({
+        street: ['', [Validators.required]],
+        number: ['', [Validators.required]],
+        block: [''],
+        apartment: [''],
+        country: ['', [Validators.required]],
+        city: ['', [Validators.required]],
+        district: ['', [Validators.required]],
+      }),
     });
 
     console.log(this.userForm);
   }
 
   onSubmit() {
+
+    console.log('Form Values:', this.userForm.value);
+    console.log('Address Values:', this.userForm.get('address')?.value);
+    const userData = {
+      name: this.userForm.get('name')?.value,
+      age: this.userForm.get('age')?.value,
+      sex: this.userForm.get('sex')?.value,
+      document: this.userForm.get('document')?.value,
+      status: this.userForm.get('status')?.value,
+      address: this.userForm.get('address')?.value,
+    };
+
     if (this.userForm.valid) {
-      console.log('Form Submitted!', this.userForm.value);
-      this.openModalCreate();
+      this.userService.createUser(userData).subscribe((response) => {
+        this.openModalCreate();
+        this.userForm.reset();
+        console.log(JSON.stringify(userData.address, null, 2));
+        console.log('Usuário criado com sucesso:', response);
+      });
     } else {
       console.log('Form is invalid');
     }
+    console.log('Form submitted:', userData);
   }
 
   openModalCreate(): void {
@@ -39,15 +67,12 @@ export class AppComponent {
     const overlay = document.querySelector('.modal-overlay') as HTMLElement;
     modal.classList.add('show');
     overlay.classList.add('show');
-  };
-  
+  }
+
   closeModalCreate(): void {
     const modal = document.querySelector('.modal-create') as HTMLElement;
     const overlay = document.querySelector('.modal-overlay') as HTMLElement;
-    if (this.userForm.valid) {
-      modal.classList.remove('show');
-      overlay.classList.remove('show');
-    }
-    this.userForm.reset(); // Reset the form after submission
+    modal.classList.remove('show');
+    overlay.classList.remove('show');
   }
 }
---
+++ src/app/user.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private apiUrl = 'http://localhost:3000/users';
+
+  constructor(private http: HttpClient) {}
+
+  createUser(userData: any): Observable<any> {
+    return this.http.post(this.apiUrl, userData);
+  }
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
